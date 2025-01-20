c1f9b01b43be1ccfdc7905cf56f748dc2db7aaed
Samuel Imina <seimina@myseneca.ca>
2025-01-20 01:55:25 -0500

    Completed Labs.

---
+++ lab2f.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+#Author: Imina Samuel Enakhe
+#AuthorID: seimina
+#Date Created: 2025/01/19
+
+import sys
+
+timer = int(sys.argv[1])
+while timer > 0:
+    print(timer)
+    timer -= 1
+print("blast off!") 
+
